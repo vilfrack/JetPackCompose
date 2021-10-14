7c390f03715e8e496df7bf1c33598c23d4c28d5d
Juan villarroel <vilfrack21@gmail.com>
2021-10-14 14:03:37 -0300

    Firts Commit

--- app/src/main/java/com/tutorialjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/tutorialjetpackcompose/MainActivity.kt
@@ -3,36 +3,29 @@ package com.tutorialjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.contentColorFor
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.tutorialjetpackcompose.ui.Componets.CardDemo
 import com.tutorialjetpackcompose.ui.theme.JetPackComposeTheme
+import java.nio.file.WatchEvent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetPackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("ddddddddddddddddddddddddddddddddddddddddddd")
-                }
-            }
+            CardDemo()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello asdasdasdasd $name!")
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetPackComposeTheme {
-        Greeting("Android")
-    }
-}
