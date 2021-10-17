37a2a74bdb4f145a1f3d16eab9ef971c6520c754
Juan villarroel <vilfrack21@gmail.com>
2021-10-16 23:21:45 -0300

    Work with rom and column

--- app/src/main/java/com/tutorialjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/tutorialjetpackcompose/MainActivity.kt
@@ -3,24 +3,43 @@ package com.tutorialjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
-import androidx.compose.material.contentColorFor
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
-import com.tutorialjetpackcompose.ui.theme.JetPackComposeTheme
-import java.nio.file.WatchEvent
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //https://youtu.be/rHKeRWK3zL4
+        setContent {
+            Column( modifier = Modifier
+                                .fillMaxSize()
+                                .background(Color.Green),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.SpaceAround
+            )
+            {
+                Text("Hello")
+                Text("World")
+                Text("plop")
+                Row(modifier = Modifier
+                                .fillMaxSize()
+                                .background(Color.Green),
+                    horizontalArrangement = Arrangement.SpaceAround,
+                    verticalAlignment = Alignment.CenterVertically) {
+                    Text("This is a")
+                    Text("Row")
+                }
+            }
+
+        }
     }
 }
 
