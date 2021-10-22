f858e94511719fbe39fef46b1ed4f2ff16a53291
Juan villarroel <vilfrack21@gmail.com>
2021-10-21 22:43:39 -0300

    delete reference

--- app/src/main/java/com/tutorialjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/tutorialjetpackcompose/MainActivity.kt
@@ -3,20 +3,7 @@ package com.tutorialjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.contentColorFor
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
-import com.tutorialjetpackcompose.ui.theme.JetPackComposeTheme
-import java.nio.file.WatchEvent
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
